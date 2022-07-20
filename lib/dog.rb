b5778640530bf708553ac6970a9e6b94010c136c
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-20 17:56:44 -0400

    completed assignment

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+  # Class body
+
+  # Instance Method Definitions
+  def bark; puts 'Woof!'; end
+  def sit; puts 'The Dog is sitting'; end
+end
+
+fido = Dog.new
+fido.bark       #=> 'Woof!'
+
+snoopy = Dog.new
+snoopy.bark       #=> 'Woof!'
